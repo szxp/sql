19ecfbc05ee9b588124f792a776c6db27146108d
Péter Szakszon <szp0530@gmail.com>
2023-05-13 07:22:13 +0200

    npv queries

--- npv_queries.sql
+++ npv_queries.sql
@@ -42,9 +42,17 @@ insert into Queries
 
 -- npv of each query
 
+-- id year  npv
+-- 1	2019	113
+-- 2	2008	121
+-- 3	2009	12
+-- 7	2018	0
+-- 7	2019	0
+-- 7	2020	30
+-- 13	2019	40
 
 -- solution 1
-select q.id, q.year, coalesce(n.npv, 0)
+select q.id, q.year, coalesce(n.npv, 0) as npv
 from Queries q
 left join NPV n on q.id = n.id and q.year = n.year
 ;
